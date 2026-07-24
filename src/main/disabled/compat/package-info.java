1e156650e1dd9539d2116736c3a9a94b3b88e78a
Lonmo <95102577+Lonmo0208@users.noreply.github.com>
2026-07-24 08:35:43 +0800

    Bump version to 4.8.4207 & fix Jade compat: split DryingRackProvider into separate client/data providers

--- src/main/disabled/compat/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package twilightforest.world.components.structures.finalcastle;
-
-import org.jspecify.annotations.NullMarked;
--- src/main/java/twilightforest/compat/jade/DryingRackDataProvider.java
+++ src/main/java/twilightforest/compat/jade/DryingRackDataProvider.java
@@ -1,29 +1,15 @@
 package twilightforest.compat.jade;
 
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import snownee.jade.api.*;
-import snownee.jade.api.config.IPluginConfig;
-import snownee.jade.api.ui.JadeUI;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IServerDataProvider;
 import twilightforest.TwilightForestMod;
 import twilightforest.block.entity.DryingRackBlockEntity;
-import twilightforest.compat.RecipeViewerConstants;
 
-public enum DryingRackProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
+public enum DryingRackDataProvider implements IServerDataProvider<BlockAccessor> {
 	INSTANCE;
-
-	@Override
-	public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
-		CompoundTag data = accessor.getServerData();
-		if (data.contains("progress")) {
-			int progress = data.getInt("progress").orElse(0);
-
-			int total = data.getInt("total").orElse(0);
-			tooltip.add(JadeUI.text(Component.translatable("jade.drying_rack.remaining", RecipeViewerConstants.getDryingTime(total - progress))));
-		}
-	}
 
 	@Override
 	public void appendServerData(CompoundTag data, BlockAccessor accessor) {
