3f157eb6617c36fcb16778d70aa459a90365057f
Lonmo <95102577+Lonmo0208@users.noreply.github.com>
2026-08-24 03:32:02 +0800

    cleanup: remove debug logging and unused code

--- src/main/java/twilightforest/world/components/feature/ThornFeature.java
+++ src/main/java/twilightforest/world/components/feature/ThornFeature.java
@@ -44,13 +44,12 @@ public class ThornFeature implements Feature {
 		int nextLength = 2 + rand.nextInt(4);
 		int maxLength = 2 + rand.nextInt(4) + rand.nextInt(4) + rand.nextInt(4);
 
-		int placed = placeThorns(world, rand, pos, nextLength, Direction.UP, maxLength, pos, config, true);
+		placeThorns(world, rand, pos, nextLength, Direction.UP, maxLength, pos, config, true);
 
 		return true;
 	}
 
-	private int placeThorns(WorldGenLevel world, RandomSource rand, BlockPos pos, int length, Direction dir, int maxLength, BlockPos oPos, ThornsConfig config, boolean avoidGiantCloud) {
-		int placed = 0;
+	private void placeThorns(WorldGenLevel world, RandomSource rand, BlockPos pos, int length, Direction dir, int maxLength, BlockPos oPos, ThornsConfig config, boolean avoidGiantCloud) {
 		boolean complete = false;
 		for (int i = 0; i < length; i++) {
 			BlockPos dPos = pos.relative(dir, i);
@@ -59,17 +58,14 @@ public class ThornFeature implements Feature {
 				if (Math.abs(dPos.getX() - oPos.getX()) < config.maxSpread() && Math.abs(dPos.getZ() - oPos.getZ()) < config.maxSpread() && canPlaceThorns(world, dPos)) {
 					world.setBlock(dPos, TFBlocks.BROWN_THORNS.defaultBlockState().setValue(RotatedPillarBlock.AXIS, dir.getAxis()), Block.UPDATE_CLIENTS);
 					world.getChunk(dPos).markPosForPostProcessing(dPos);
-					placed++;
 
 					if (i == length - 1) {
 						complete = true;
 						if (rand.nextInt(config.chanceOfLeaf()) == 0 && world.isEmptyBlock(dPos.relative(dir))) {
 							if (rand.nextInt(config.chanceLeafIsRose()) > 0) {
 								world.setBlock(dPos.relative(dir), TFBlocks.THORN_LEAVES.defaultBlockState().setValue(LeavesBlock.DISTANCE, 1), Block.UPDATE_ALL);
-								placed++;
 							} else {
 								world.setBlock(dPos.relative(dir), TFBlocks.THORN_ROSE.defaultBlockState().setValue(DirectionalBlock.FACING, dir), Block.UPDATE_ALL);
-								placed++;
 							}
 						}
 					}
@@ -85,7 +81,7 @@ public class ThornFeature implements Feature {
 			Direction nextDir = Direction.getRandom(rand);
 			BlockPos nextPos = pos.relative(dir, length - 1).relative(nextDir);
 			int nextLength = 1 + rand.nextInt(maxLength);
-			placed += this.placeThorns(world, rand, nextPos, nextLength, nextDir, maxLength - 1, oPos, config, false);
+			this.placeThorns(world, rand, nextPos, nextLength, nextDir, maxLength - 1, oPos, config, false);
 		}
 
 		if (complete && length > 3 && rand.nextInt(config.chanceOfBranch()) == 0) {
@@ -93,7 +89,7 @@ public class ThornFeature implements Feature {
 			Direction nextDir = Direction.getRandom(rand);
 			BlockPos nextPos = pos.relative(dir, middle).relative(nextDir);
 			int nextLength = 1 + rand.nextInt(maxLength);
-			placed += this.placeThorns(world, rand, nextPos, nextLength, nextDir, maxLength - 1, oPos, config, false);
+			this.placeThorns(world, rand, nextPos, nextLength, nextDir, maxLength - 1, oPos, config, false);
 		}
 
 		if (complete && length > 3 && rand.nextInt(config.chanceOfLeaf()) == 0) {
@@ -102,11 +98,8 @@ public class ThornFeature implements Feature {
 			BlockPos nextPos = pos.relative(dir, middle).relative(nextDir);
 			if (world.isEmptyBlock(nextPos)) {
 				world.setBlock(nextPos, TFBlocks.THORN_LEAVES.defaultBlockState().setValue(LeavesBlock.DISTANCE, 1), Block.UPDATE_ALL);
-				placed++;
 			}
 		}
-
-		return placed;
 	}
 
 	private static boolean checkIsUnderCloud(WorldGenLevel world, BlockPos pos, BlockPos dPos) {
--- src/main/java/twilightforest/world/components/placements/ChunkBlanketingModifier.java
+++ src/main/java/twilightforest/world/components/placements/ChunkBlanketingModifier.java
@@ -15,17 +15,12 @@ import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.placement.PlacementContext;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Optional;
 import java.util.function.Consumer;
 
 // Ideally, you should not be mixing this with other decorators unless you know what you're doing
 // This litters memory with 256 block positions for each chunk. USE SPARINGLY
 public class ChunkBlanketingModifier implements PlacementModifier {
-	private static final Logger LOGGER = LoggerFactory.getLogger("twilightforest");
-
 	public static final MapCodec<ChunkBlanketingModifier> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
 		Codec.floatRange(0.0f, 1.0f).fieldOf("integrity").forGetter(o -> o.integrity),
 		Heightmap.Types.CODEC.fieldOf("heightmap").forGetter(o -> o.heightmap),
@@ -54,28 +49,19 @@ public class ChunkBlanketingModifier implements PlacementModifier {
 		int chunkOriginX = chunk.getPos().getMinBlockX();
 		int chunkOriginZ = chunk.getPos().getMinBlockZ();
 
-		int count = 0;
 		BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
 		for (int zInChunk = 0; zInChunk < 16; zInChunk++) {
 			for (int xInChunk = 0; xInChunk < 16; xInChunk++) {
 				if (random.nextFloat() > this.integrity)
 					continue;
 
-				// Manually scan for the surface height instead of using chunk.getHeight
-				// This is needed because the twilight forest dimension's custom surface building
-				// may not properly populate the heightmap in 26.3
 				int surfaceY = findSurfaceHeight(chunk, level, chunkOriginX + xInChunk, chunkOriginZ + zInChunk);
 				BlockPos pos = new BlockPos(chunkOriginX + xInChunk, surfaceY + 1, chunkOriginZ + zInChunk);
 
 				if (this.biomeRLOptional.isEmpty() || this.biomeRLOptional.get().contains(level.getBiome(pos))) {
 					collector.accept(pos);
-					count++;
 				}
 			}
-		}
-
-		if (count > 0) {
-			LOGGER.info("[ThornsDebug] ChunkBlanketingModifier placed {} positions at chunk {} (integrity={}, biomeLock={})", count, chunk.getPos(), this.integrity, this.biomeRLOptional.isPresent());
 		}
 	}
 
@@ -83,7 +69,6 @@ public class ChunkBlanketingModifier implements PlacementModifier {
 		int maxY = level.getMaxY();
 		int minY = level.getMinY();
 
-		// Scan from top down to find the highest non-air block
 		BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
 		for (int y = maxY; y >= minY; y--) {
 			mutablePos.set(blockX, y, blockZ);
